202dfcc5b5965a5bad821ea512a7404becdd83e8
Rusbelito <rusbelito@gmail.com>
2026-01-04 19:54:29 -0500

    app base añadido

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/AppBase-Front-SPA",
   plugins: [
     vue(),
     vueDevTools(),
