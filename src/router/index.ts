6ba72dddd55077e9b9d9e85c08138f578a340252
Rusbelito <rusbelito@gmail.com>
2026-01-04 21:43:57 -0500

    ajuste sensillo

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const Dashboard = () => import('../views/Dashboard.vue')
 const SelectContext = () => import('../views/auth/SelectContext.vue')
 
 const router = createRouter({
-  history: createWebHistory('#'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
