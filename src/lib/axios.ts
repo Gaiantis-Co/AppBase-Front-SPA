6347dbf23ae174fbeef3d5b22d54b9b8c1cb4dfe
Rusbelito <rusbelito@gmail.com>
2026-01-05 21:58:03 -0500

    test https

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import notify from './toast'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://appbase-back.test',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'https://appbase-back.test',
   withCredentials: true,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
